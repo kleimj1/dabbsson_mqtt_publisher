345a1fd598a2ed981f26718668434156c61d22be
kleimj1 <jochen.kleimann@gmail.com>
2025-04-06 23:33:23 +0200

    Update run.sh

--- dabbsson_mqtt_publisher/run.sh
+++ dabbsson_mqtt_publisher/run.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
+export DEVICE_ID=$(jq -r .device_id /data/options.json)
+export LOCAL_KEY=$(jq -r .local_key /data/options.json)
+export IP=$(jq -r .ip /data/options.json)
+export MQTT_HOST=$(jq -r .mqtt_host /data/options.json)
+export MQTT_PORT=$(jq -r .mqtt_port /data/options.json)
+export MQTT_TOPIC=$(jq -r .mqtt_topic /data/options.json)
+export MQTT_COMMAND_TOPIC=$(jq -r .mqtt_command_topic /data/options.json)
+export MQTT_DISCOVERY_PREFIX=$(jq -r .mqtt_discovery_prefix /data/options.json)
 
-export DEVICE_ID=$(bashio::config 'device_id')
-export LOCAL_KEY=$(bashio::config 'local_key')
-export IP=$(bashio::config 'ip')
-export MQTT_HOST=$(bashio::config 'mqtt_host')
-export MQTT_PORT=$(bashio::config 'mqtt_port')
-export MQTT_TOPIC=$(bashio::config 'mqtt_topic')
-export MQTT_COMMAND_TOPIC=$(bashio::config 'mqtt_command_topic')
-export MQTT_DISCOVERY_PREFIX=$(bashio::config 'mqtt_discovery_prefix')
-
-python3 /mqtt_publisher.py
+/usr/bin/python3 /mqtt_publisher.py
