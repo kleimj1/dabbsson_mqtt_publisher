9010865fa9d16539b06db2d24d694fb50e7f3ebb
kleimj1 <jochen.kleimann@gmail.com>
2025-04-07 11:26:45 +0200

    Update run.sh

--- dabbsson_mqtt_publisher/run.sh
+++ dabbsson_mqtt_publisher/run.sh
@@ -2,7 +2,7 @@
 
 export DEVICE_ID=$(bashio::config 'device_id')
 export LOCAL_KEY=$(bashio::config 'local_key')
-export IP=$(bashio::config 'device_ip')
+export DEVICE_IP=$(bashio::config 'device_ip')
 export MQTT_HOST=$(bashio::config 'mqtt_host')
 export MQTT_PORT=$(bashio::config 'mqtt_port')
 export MQTT_TOPIC=$(bashio::config 'mqtt_topic')
@@ -11,4 +11,5 @@ export MQTT_DISCOVERY_PREFIX=$(bashio::config 'mqtt_discovery_prefix')
 export MQTT_USER=$(bashio::config 'mqtt_user')
 export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
 
-exec python3 /mqtt_publisher.py
+# Starte den Publisher über Python aus der venv
+/opt/venv/bin/python /mqtt_publisher.py
