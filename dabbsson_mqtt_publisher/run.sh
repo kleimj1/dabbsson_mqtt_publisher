7003d5e96834620281c190e00ed87e5fea6fd1ee
kleimj1 <jochen.kleimann@gmail.com>
2025-04-07 11:17:45 +0200

    Update run.sh

--- dabbsson_mqtt_publisher/run.sh
+++ dabbsson_mqtt_publisher/run.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 
-# Konfigwerte lesen
-export DEVICE_ID=$(jq -r .device_id /data/options.json)
-export LOCAL_KEY=$(jq -r .local_key /data/options.json)
-export IP=$(jq -r .device_ip /data/options.json)
-export MQTT_HOST=$(jq -r .mqtt_host /data/options.json)
-export MQTT_PORT=$(jq -r .mqtt_port /data/options.json)
-export MQTT_TOPIC=$(jq -r .mqtt_topic /data/options.json)
-export MQTT_COMMAND_TOPIC=$(jq -r .mqtt_command_topic /data/options.json)
-export MQTT_DISCOVERY_PREFIX=$(jq -r .mqtt_discovery_prefix /data/options.json)
-export MQTT_USER=$(jq -r .mqtt_user /data/options.json)
-export MQTT_PASSWORD=$(jq -r .mqtt_password /data/options.json)
+export DEVICE_ID=$(bashio::config 'device_id')
+export LOCAL_KEY=$(bashio::config 'local_key')
+export IP=$(bashio::config 'device_ip')
+export MQTT_HOST=$(bashio::config 'mqtt_host')
+export MQTT_PORT=$(bashio::config 'mqtt_port')
+export MQTT_TOPIC=$(bashio::config 'mqtt_topic')
+export MQTT_COMMAND_TOPIC=$(bashio::config 'mqtt_command_topic')
+export MQTT_DISCOVERY_PREFIX=$(bashio::config 'mqtt_discovery_prefix')
+export MQTT_USER=$(bashio::config 'mqtt_user')
+export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
 
-# Starte das Script im venv
-exec /data/venv/bin/python /mqtt_publisher.py
+exec python3 /mqtt_publisher.py
